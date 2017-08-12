e6db45ba375194368e3e3d138bd46b344cb520c5
Paul Water <p.r.water@setbased.nl>
2017-08-12 15:51:58 +0200

    Added method getCmpId.

--- src/DomainResolver.php
+++ src/DomainResolver.php
@@ -10,7 +10,7 @@ interface DomainResolver
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Returns the domain (a.k.a. company abbreviation).
+   * Returns the abbreviation of the company (a.k.a. domain).
    *
    * @return string
    *
@@ -18,6 +18,17 @@ interface DomainResolver
    * @since 1.0.0
    */
   public function getDomain();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of the company (a.k.a. domain).
+   *
+   * @return int
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function getCmpId();
 
   //--------------------------------------------------------------------------------------------------------------------
 }
